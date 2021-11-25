f884208070b3961be27473bef42cd9f1ae2baa3c
Roman Boyarsky <boyarsky1997@gmail.com>
2021-11-25 22:22:27 +0200

    added class RandomString to generate a random string

---
+++ src/main/java/com/github/boyarsky1997/techtask/RandomString.java
@@ -0,0 +1,35 @@
+package com.github.boyarsky1997.techtask;
+
+import java.util.Random;
+
+public class RandomString {
+
+    private static final char[] DICTIONARY =
+            "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm".toCharArray();
+    private static final int MIN_VALUE = 2;
+    private static final int MAX_VALUE = 10;
+
+    private final Random random;
+
+    public RandomString() {
+        this.random = new Random();
+    }
+
+    public RandomString(long seed) {
+        this.random = new Random(seed);
+    }
+
+    public String nextString() {
+        return nextString(MIN_VALUE, MAX_VALUE);
+    }
+
+    public String nextString(int min, int max) {
+        int length = random.nextInt(max - min) + min;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i <= length; i++) {
+            result.append(DICTIONARY[random.nextInt(DICTIONARY.length - 1)]);
+        }
+        return result.toString();
+    }
+
+}
